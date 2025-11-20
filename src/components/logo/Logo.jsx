9b1ec1f8b28871b51b443f8ffb20193399cbd164
Lucas Patrício <lucaspatricio.8@gmail.com>
2025-11-20 18:47:57 -0300

    Ajuste da Logo do app

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -1,11 +1,10 @@
 import { AiOutlineAim } from "react-icons/ai";
 
-
-function Logo ({ size = "text-2xl" }) {
-    return(
-        <div className={`flex items-center text-white font-bold ${size} whitespace-nowrap`}>
+function Logo({ size = "text-2xl" }) {
+    return (
+        <div className={`flex items-center justify-center text-white font-bold ${size} whitespace-nowrap`}>
             F
-            <AiOutlineAim className="mx-0 text-blue-500 padding"/>
+            <AiOutlineAim className="text-blue-500 mx-0.5 self-center" />
             CUS
         </div>
     )

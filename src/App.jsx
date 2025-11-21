d5b700ee2238eca7474e6f392390f463634de9a0
Pedro Moraes <pedro693.ph@gmail.com>
2025-11-20 23:35:44 -0300

    refactor: ajusta chamada do path para pagina de musicas curtidas

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
               }
             />
             <Route 
-              path='/favoritos'
+              path='/curtidos'
               element={
                 <MainLayout>
                   <LayoutWithSidebar>
